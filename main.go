c046dfcd8ec72c2dc0855ea9c021236261d635a6
Shawn Smith <shawnpsmith@gmail.com>
2013-11-23 00:41:35 +0900

    start on search

--- main.go
+++ main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/gorilla/mux"
 	"github.com/kylelemons/go-gypsy/yaml"
 	"github.com/shawnps/gr"
 	"github.com/shawnps/rt"
 	"github.com/shawnps/sp"
+	"html/template"
 	"log"
 	"net/http"
+	"net/url"
 	"strings"
 )
 
@@ -36,21 +39,54 @@ func parseYAML() (rtKey, grKey, grSecret string) {
 }
 
 // Search Rotten Tomatoes, Goodreads, and Spotify.
-func Search(q string) {
+func Search(q string, rt rt.RottenTomatoes, gr gr.Goodreads, sp sp.Spotify) ([]rt.Movie, gr.GoodreadsResponse, sp.SearchAlbumsResponse) {
+	m, err := rt.SearchMovies(q)
+	if err != nil {
+		fmt.Println("ERROR (rt): ", err.Error())
+	}
+	g, err := gr.SearchBooks(q)
+	if err != nil {
+		fmt.Println("ERROR (gr): ", err.Error())
+	}
+	s, err := sp.SearchAlbums(q)
+	if err != nil {
+		fmt.Println("ERROR (sp): ", err.Error())
+	}
+	return m, g, s
 }
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
+}
+
+func SearchHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	q := vars["query"]
+	q, err := url.QueryUnescape(q)
+	if err != nil {
+		log.Panic(err)
+	}
 	rtKey, grKey, grSecret := parseYAML()
 	rt := rt.RottenTomatoes{rtKey}
 	gr := gr.Goodreads{grKey, grSecret}
 	sp := sp.Spotify{}
-	fmt.Println(rt)
-	fmt.Println(gr)
-	fmt.Println(sp)
-	fmt.Fprintf(w, "Hello")
+	m, g, s := Search(q, rt, gr, sp)
+	t, err := template.New("search.html").ParseFiles("templates/search.html")
+	if err != nil {
+		log.Panic(err)
+	}
+	// Render the template
+	err = t.Execute(w, map[string]interface{}{"Movies": m, "Books": g, "Albums": s})
+	if err != nil {
+		log.Panic(err)
+	}
 }
 
 func main() {
-	http.HandleFunc("/", HomeHandler)
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/search/{query}", SearchHandler)
+	http.Handle("/", r)
+	fmt.Println("Running on localhost:" + *port)
+
 	log.Fatal(http.ListenAndServe(":"+*port, nil))
 }
