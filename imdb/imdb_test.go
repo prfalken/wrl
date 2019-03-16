45c7f0f8028ea5ec23faf80419fe5bd940665762
Julien Dehee <julien.dehee@gmail.com>
2019-03-17 00:45:15 +0100

    add env variable support for imdb key

--- imdb/imdb_test.go
+++ imdb/imdb_test.go
@@ -1,21 +1,27 @@
 package imdb
 
 import (
+	"os"
 	"testing"
 
 	"github.com/kylelemons/go-gypsy/yaml"
 )
 
 func TestImdb(t *testing.T) {
-	config, err := yaml.ReadFile("config_test.yml")
-	if err != nil {
-		t.Fatal("Could not read config file")
-	}
-	imdbKey, err := config.Get("imdb.key")
-	if err != nil {
-		t.Fatal("could not get imdb key from config file")
-	}
+	imdbKey := ""
+	if os.Getenv("IMDB_KEY") != "" {
+		imdbKey = os.Getenv("IMDB_KEY")
+	} else {
+		config, err := yaml.ReadFile("config_test.yml")
+		if err != nil {
+			t.Log("Could not read config file")
 
+		}
+		imdbKey, err = config.Get("imdb.key")
+		if err != nil {
+			t.Fatal("could not get imdb key from config file")
+		}
+	}
 	i := Imdb{Key: imdbKey}
 	resp, err := i.SearchMovies("Home Alone")
 
